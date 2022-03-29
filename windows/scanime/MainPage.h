99a81a0dc7f4dfec71e57cdc2e961f94032a8fd4
Sebastian Cerviño <cervisebas@gmail.com>
2022-03-29 20:38:40 -0300

    29/03/2022 20:39hs

---
+++ windows/scanime/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::scanime::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::scanime::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
